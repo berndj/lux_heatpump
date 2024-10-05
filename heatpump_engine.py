f673294283b34a2237c010594418fa4ed7219468
Bernd Jungblut <bernd.jungblut@gmail.com>
2024-10-05 19:22:41 +0200

    safer token detection

--- heatpump_engine.py
+++ heatpump_engine.py
@@ -9,12 +9,12 @@ class heatpump_engine:
 
     def __init__(self):
         """Init heatpump connection."""
-        self.heating_circuit_flow_temp = 0.0
-        self.heating_circuit_return_flow_temp_actual = 0.0
-        self.heating_circuit_return_flow_temp_setpoint = 0.0
-        self.domestic_hot_water_temp_setpoint = 0.0
-        self.domestic_hot_water_temp_actual = 0.0
-        self.outdoor_temp = 0.0
+        self.heating_circuit_flow_temp = None
+        self.heating_circuit_return_flow_temp_actual = None
+        self.heating_circuit_return_flow_temp_setpoint = None
+        self.domestic_hot_water_temp_setpoint = None
+        self.domestic_hot_water_temp_actual = None
+        self.outdoor_temp = None
         self.polls = 0
         self.polls_skipped = 0
         self.epoch_time = int(time.time())
@@ -31,70 +31,91 @@ class heatpump_engine:
     def maintain_socket(self, host, port):
         """Check and repair socket."""
 
-        if self.is_socket_closed(self.sock) or port != self.port or host != self.host:
+        if True == self.is_socket_closed(self.sock) or port != self.port or host != self.host:
             self.align_peer(host, port)
             self.sock.close()
             self.sock = None
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock.connect((self.host, self.port))
+            self.sock.settimeout(0.1)
+            if -1 == self.connect():
+                return -1
+        return 0
 
     def poll_for_stats(self, host, port):
         """Poll sensor data."""
 
         new_time = int(time.time())
         if new_time - self.epoch_time > 5 or self.polls == 0:
-            self.maintain_socket(host, port)
-            self.trigger_stats()
-            self.readlines()
-            self.epoch_time = new_time
-            self.polls += 1
+            if 0 == self.maintain_socket(host, port):
+                if 0 != self.trigger_stats():
+                    return -1
+                self.readlines()
+                self.epoch_time = new_time
+                self.polls += 1
         else:
             self.polls_skipped += 1
 
     def is_socket_closed(self, sock: socket.socket) -> bool:
         """Check socket closed state."""
+        sock.settimeout(0.1)
         try:
             # this will try to read bytes without blocking and also without removing them from buffer (peek only)
             data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
             if len(data) == 0:
-                return True
+                return False
         except BlockingIOError:
-            return False  # socket is open and reading from it would block
+            return True  # socket is open and reading from it would block
         except ConnectionResetError:
             return True  # socket was closed for some other reason
         except Exception:  # noqa: BLE001
-            return False  # socket was closed for some other reason
-        return False
+            return True  # socket was closed for some other reason
+        return True
 
     def connect(self):
         """Connect to ser2net socket."""
         try:
             self.sock.connect((self.host, self.port))
-        except ConnectionError:
-            # print("Connection Failed")
-            return
+        except TimeoutError:
+            print('oops timeout')
+            return -1
+        except ConnectionAbortedError:
+            print('oops abortr')
+            return -1
+        except socket.gaierror:
+            print('oops gaierror')
+            return -1
+        print('connect ok')
+        return 0
 
     def readlines(self):
         """Read answer from ser2net/heatpump."""
-        self.sock.settimeout(0.1)
+        data = b""
+        self.sock.settimeout(0.5)
         while True:
             try:
-                data = self.sock.recv(1024)
+                new_data = self.sock.recv(1024)
             except TimeoutError:
                 break
             except ConnectionAbortedError:
                 break
-            lines = data.split(b"\r\n")
-            if len(data) == 0:
+            data += new_data
+            if len(new_data) == 0:
                 break
-            for line in lines:
-                self.extract_temp(line)
 
+        lines = data.split(b"\r\n")
+        for line in lines:
+            self.extract_temp(line)
+
+                
     def trigger_stats(self):
         """Trigger response from heatpump."""
         # buf = "1800\n\r" # most stats
         buf = "1100\n\r"  # temperature stats only
-        self.sock.send(buf.encode(encoding="utf-8"))
+        try:
+            self.sock.send(buf.encode(encoding="utf-8"))
+        except BrokenPipeError:
+            return -1
+        return 0
 
     def extract_temp(self, line):
         """Extract temperature values from response."""
@@ -104,59 +125,61 @@ class heatpump_engine:
         try:
             cat1 = int(tokens[0])
             if len(tokens) > 1:
-                cat2 = int(tokens[1])
+                nr_tokens = int(tokens[1])
             else:
                 return
         except ValueError:
             return
 
-        if cat1 == 1100 and cat2 == 12 and len(tokens) >= 9:
+        if cat1 == 1100 and nr_tokens == 12 and len(tokens) == nr_tokens+2:
+            for token in tokens:
+                print(token)
             tokens.pop(0)
             tokens.pop(0)
-            #            for token in tokens:
-            #                print(token)
+            print(ser_str)
             try:
-                self.heating_circuit_flow_temp = float(tokens[0]) / 10
-                self.heating_circuit_return_flow_temp_actual = float(tokens[1]) / 10
-                self.heating_circuit_return_flow_temp_setpoint = float(tokens[2]) / 10
-                self.outdoor_temp = float(tokens[4]) / 10
-                self.domestic_hot_water_temp_actual = float(tokens[5]) / 10
-                self.domestic_hot_water_temp_setpoint = float(tokens[6]) / 10
+                self.heating_circuit_flow_temp = float(tokens[0]) / 10.0
+                self.heating_circuit_return_flow_temp_actual = float(tokens[1]) / 10.0
+                self.heating_circuit_return_flow_temp_setpoint = float(tokens[2]) / 10.0
+                self.outdoor_temp = float(tokens[4]) / 10.0
+                self.domestic_hot_water_temp_actual = float(tokens[5]) / 10.0
+                self.domestic_hot_water_temp_setpoint = float(tokens[6]) / 10.0
             except ValueError:
                 return
 
-
-# pylint: disable=pointless-string-statement'
-"""
-        def print_sensors(self):
+    def print_sensors(self):
         print(
             "=============================================================================="
         )
         print()
         print("Poll=" + str(self.polls) + " Polls_skipped=" + str(self.polls_skipped))
-        print("Outdoor temperature\t\t\t\t\t = %.1f" % (self.outdoor_temp))
+        print("Outdoor temperature\t\t\t\t\t = %s" % (str(self.outdoor_temp)))
         print()
         print(
-            "heating circuit flow temperature \t\t\t = %.1f"
-            % (self.heating_circuit_flow_temp)
+            "heating circuit flow temperature \t\t\t = %s"
+            % (str(self.heating_circuit_flow_temp))
         )
         print(
-            "heating circuit return flow temperature (actual)\t = %.1f"
+            "heating circuit return flow temperature (actual)\t = %s"
             % (self.heating_circuit_return_flow_temp_actual)
         )
         print(
-            "heating circuit return flow temperature (setpoint)\t = %.1f"
+            "heating circuit return flow temperature (setpoint)\t = %s"
             % (self.heating_circuit_return_flow_temp_setpoint)
         )
         print(
-            "domestic hot water temperature (actual)\t\t\t = %.1f"
+            "domestic hot water temperature (actual)\t\t\t = %s"
             % (self.domestic_hot_water_temp_actual)
         )
         print(
-            "domestic hot water temperature (setpoint)\t\t = %.1f"
+            "domestic hot water temperature (setpoint)\t\t = %s"
             % (self.domestic_hot_water_temp_setpoint)
         )
-"""
+        print(
+            "=============================================================================="
+        )
+
+
 
 my_heatpump_engine = heatpump_engine()
 
@@ -164,6 +187,12 @@ my_heatpump_engine = heatpump_engine()
 if __name__ == "__main__":
     conn = heatpump_engine()
     while True:
-        conn.poll_for_stats("baba-cafe", 4322)
-        #        conn.print_sensors()
+        conn.poll_for_stats("baba-cafe.local", 4322)
+        conn.print_sensors()
         time.sleep(4)
+
+
+
+
+
+        
