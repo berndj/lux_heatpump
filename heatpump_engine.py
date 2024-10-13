ea02851d30b7f63d97b98458e234da276a6fb9fa
Bernd Jungblut <bernd.jungblut@googlemail.com>
2024-10-14 00:02:35 +0200

    move heat pump type enum to const module

--- heatpump_engine.py
+++ heatpump_engine.py
@@ -6,57 +6,17 @@ import socket
 import time
 import re
 
-from const import POLL_INTERVAL
+from const import HeatPumpType, POLL_INTERVAL
 
-class HeatPumpType(IntEnum):
-    """Heatpump type."""
-    ERC = 0
-    SW1 = 1
-    SW2 = 2
-    WW1 = 3
-    WW2 = 4
-    L1I = 5
-    L2I = 6
-    L1A = 7
-    L2A = 8
-    KSW = 9
-    KLW = 10
-    SWC = 11
-    LWC = 12
-    L2G = 13
-    WZS =14
-    L1I407 = 15
-    L2I407 = 16
-    L1A407 = 17
-    L2A407 = 18
-    L2G407 = 19
-    LWC407 = 20
-    L1AREV = 21
-    L2AREV = 22
-    WWC1 = 23
-    WWC2 = 24
-    L2G404 = 25
-    WZW = 26
-    L1S = 27
-    L1H = 28
-    L2H = 29
-    WZWD = 30
-    ERC = 31
-    WWB_20 = 40
-    LD5 = 41
-    LD7 = 42
-    UNKNOWN = -1
 
 class HeatPumpMode(IntEnum):
     """Heatpump mode."""
-
     AUTO = 0
     ZWE = 1
     PARTY = 2
     VACATION = 3
     OFF = 4
     UNKNOWN = 5
-
 
 class HeatPumpFunction(IntEnum):
     """Heatpump functions."""
@@ -105,11 +65,6 @@ class heatpump_engine:
         self.main_sys_uptime = datetime.fromisoformat('2000-01-01T00:05:23')
         self.main_compact = -1
         self.main_comfort = -1
-        
-        
-        
-        
-        
 
     def align_peer(self, host, port):
         """Update host and port information."""
