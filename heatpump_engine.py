612f9a00260ebb08e2d978088f9289a7645b73b8
Bernd Jungblut <bernd.jungblut@googlemail.com>
2024-10-13 23:27:41 +0200

    add enum for heatpump type

--- heatpump_engine.py
+++ heatpump_engine.py
@@ -8,6 +8,44 @@ import re
 
 from const import POLL_INTERVAL
 
+class HeatPumpType(IntEnum):
+    """Heatpump type."""
+    ERC = 0
+    SW1 = 1
+    SW2 = 2
+    WW1 = 3
+    WW2 = 4
+    L1I = 5
+    L2I = 6
+    L1A = 7
+    L2A = 8
+    KSW = 9
+    KLW = 10
+    SWC = 11
+    LWC = 12
+    L2G = 13
+    WZS =14
+    L1I407 = 15
+    L2I407 = 16
+    L1A407 = 17
+    L2A407 = 18
+    L2G407 = 19
+    LWC407 = 20
+    L1AREV = 21
+    L2AREV = 22
+    WWC1 = 23
+    WWC2 = 24
+    L2G404 = 25
+    WZW = 26
+    L1S = 27
+    L1H = 28
+    L2H = 29
+    WZWD = 30
+    ERC = 31
+    WWB_20 = 40
+    LD5 = 41
+    LD7 = 42
+    UNKNOWN = -1
 
 class HeatPumpMode(IntEnum):
     """Heatpump mode."""
@@ -60,7 +98,7 @@ class heatpump_engine:
         self.port = None
         self.hot_water_mode = HeatPumpMode.UNKNOWN
         # gen status record
-        self.main_wp_type = -1
+        self.main_wp_type = HeatPumpType.UNKNOWN
         self.main_sw_status = "unknown"
         self.main_biv_level = -1
         self.main_status = HeatPumpGenStatus.UNKNOWN
@@ -276,7 +314,7 @@ class heatpump_engine:
             tokens.pop(0)
             tokens.pop(0)
             # print(ser_str)
-            self.main_wp_type = int(tokens[0])
+            self.main_wp_type = HeatPumpType(int(tokens[0]))
             self.main_sw_status = str(tokens[1]).strip()
             self.main_biv_level = int(tokens[2])
             self.main_status = HeatPumpGenStatus(int(tokens[3]))
@@ -326,7 +364,7 @@ class heatpump_engine:
         print("")
         print("General status:")
         print("===============")
-        print("Heat pump type: \t" + str(self.main_wp_type))
+        print("Heat pump type: \t" + str(self.main_wp_type.name))
         print("System sw-version: \t[" + str(self.main_sw_status) + "]")
         print("BIV level: \t" + str(self.main_biv_level))
         print("Operational status: \t" + str(self.main_status.name))
